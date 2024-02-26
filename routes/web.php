2269ec19c6369a8b37c6c5cd8a5e0253aee0d30f
bilaldarmawan <159266793+bilaldarmawan@users.noreply.github.com>
2024-02-27 04:36:35 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ListBarangController;
-use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\DashboardController;
 
 /*
