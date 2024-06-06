312eb33bd75e9b3d394c5145aed225c143e6973a
bilaldarmawan <bilaldarmawan@gmail.com>
2024-06-06 21:31:29 +0700

    update

--- database/migrations/2024_06_06_072656_create_users_table.php
+++ database/migrations/2024_06_06_072656_create_users_table.php
@@ -4,21 +4,25 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCartsTable extends Migration
+class CreateUsersTable extends Migration
 {
     public function up()
-    {
-        Schema::create('carts', function (Blueprint $table) {
-            $table->id();
-            $table->unsignedBigInteger('user_id');
-            $table->timestamps();
+{
+    Schema::dropIfExists('users'); // Tambahkan baris ini
 
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-        });
-    }
+    Schema::create('users', function (Blueprint $table) {
+        $table->id();
+        $table->string('name');
+        $table->string('email')->unique();
+        $table->timestamp('email_verified_at')->nullable();
+        $table->string('password');
+        $table->rememberToken();
+        $table->timestamps();
+    });
+}
 
     public function down()
     {
-        Schema::dropIfExists('carts');
+        Schema::dropIfExists('users');
     }
 }
